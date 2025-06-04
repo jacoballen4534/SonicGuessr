b75f24e7eb774a5eed9476484c08636d337ebcb7
Jacob <jacoballen4534@gmail.com>
2025-06-04 23:07:03 +1200

    angular cli flag, update prod env var

--- sonicguessr-frontend/src/environments/environment.prod.ts
+++ sonicguessr-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:3000/api' // Or your production API URL
+  apiBaseUrl: 'https://your-raspberry-pi-domain.com/api' // Or your production API URL
 };
