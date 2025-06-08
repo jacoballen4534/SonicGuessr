bdb02a538ee74e8f6bff176d3661dc703418b64b
Jacob <jacoballen4534@gmail.com>
2025-06-08 22:57:13 +1200

    Enable secure cookies again

--- song-guesser-api/server.js
+++ song-guesser-api/server.js
@@ -46,7 +46,7 @@ async function main() {
         resave: false,
         saveUninitialized: false,
         cookie: {
-            secure: false, //process.env.NODE_ENV === 'production',
+            secure: process.env.NODE_ENV === 'production',
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000,
             // sameSite: 'lax' // Consider for CSRF protection
